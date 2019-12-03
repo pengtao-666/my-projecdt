fd334eed87d4e9a439836a7706837962d645a48c
pengtao <15128667802@163.com>
2019-12-03 18:31:53 +0800

    修改hash

--- src/router/index.js
+++ src/router/index.js
@@ -46,7 +46,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: process.env.NODE_ENV === 'development' ? 'hash' : 'history',
+  mode: 'hash',
   // mode:'history',
   base: process.env.BASE_URL,
   routes
