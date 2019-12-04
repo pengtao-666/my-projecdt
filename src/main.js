9379313de8beb5e5314b288bd8b39756ee95d55f
pengtao <15128667802@163.com>
2019-12-04 11:29:31 +0800

    打包优化

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-
+import store from './store'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
-Vue.use(ElementUI)
+if (!window.ELEMENT) Vue.use(ElementUI)
 
 new Vue({
   router,
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const path = require('path')
+const isProduction = process.env.NODE_ENV !== 'development'
+const devNeedCdn = false
+
+const cdn = {
+  externals: {
+    vue: 'Vue',
+    vuex: 'Vuex',
+    'vue-router': 'VueRouter',
+    'element-ui': 'ELEMENT'
+  },
+  css: ['https://unpkg.com/element-ui/lib/theme-chalk/index.css'],
+  js: [
+    'https://cdn.bootcss.com/vue/2.6.10/vue.min.js',
+    'https://cdn.bootcss.com/vuex/3.1.1/vuex.min.js',
+    'https://cdn.bootcss.com/vue-router/3.1.3/vue-router.min.js',
+    'https://unpkg.com/element-ui/lib/index.js'
+  ]
+}
 
 function resolve (dir) {
   return path.join(__dirname, dir)
@@ -6,10 +24,19 @@ function resolve (dir) {
 module.exports = {
   publicPath: './',
   outputDir: './server/public/dist',
-
+  productionSourceMap: false,
   chainWebpack: (config) => {
+    // 配置路径
     config.resolve.alias
       .set('@', resolve('./src'))
       .set('com', resolve('./src/components'))
+      .end()
+    config.plugin('html').tap(args => {
+      if (isProduction || devNeedCdn) args[0].cdn = cdn
+      return args
+    })
+  },
+  configureWebpack: config => {
+    if (isProduction || devNeedCdn) config.externals = cdn.externals
   }
 }
